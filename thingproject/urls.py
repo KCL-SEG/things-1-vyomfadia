e5c24236f743fc73de725fafb4af4b56aa24544c
Vyom Fadia <vyom@tryterra.co>
2023-12-01 15:07:35 +0000

    feat: implement first task

--- thingproject/urls.py
+++ thingproject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import things.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', things.views.home),
 ]
