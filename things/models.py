5cd9861285620df27ba7882a228ec115a93d833e
Vyom Fadia <vyom@tryterra.co>
2023-12-04 14:56:06 +0000

    fix: why does django do things so badly

--- things/models.py
+++ things/models.py
@@ -7,5 +7,5 @@ class Thing(models.Model):
     """The basic thing model that is required for us to implement."""
 
     name = models.CharField(unique=True, max_length=30)
-    description = models.TextField(null=True, validators=[MaxLengthValidator(120)])
+    description = models.TextField(blank=True, validators=[MaxLengthValidator(120)])
     quantity = models.IntegerField(validators=[MaxValueValidator(100), MinValueValidator(0)])
