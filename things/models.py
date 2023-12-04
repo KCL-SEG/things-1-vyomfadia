e5767df67d3e18f4cda788226b648266a9c937c9
Vyom Fadia <vyom@tryterra.co>
2023-12-04 14:50:29 +0000

    what

--- things/models.py
+++ things/models.py
@@ -7,5 +7,5 @@ class Thing(models.Model):
     """The basic thing model that is required for us to implement."""
 
     name = models.CharField(unique=True, max_length=30)
-    description = models.TextField(max_length=120, null=True)
+    description = models.TextField(max_length=120)
     quantity = models.IntegerField(validators=[MaxValueValidator(99), MinValueValidator(1)])
