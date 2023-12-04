44eb674c418970a846ac2bb725e47e922126df3f
Vyom Fadia <vyom@tryterra.co>
2023-12-04 14:54:24 +0000

    fix: django is not cool

--- things/models.py
+++ things/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator, MaxLengthValidator
 
 
 # Create your models here.
@@ -7,5 +7,5 @@ class Thing(models.Model):
     """The basic thing model that is required for us to implement."""
 
     name = models.CharField(unique=True, max_length=30)
-    description = models.TextField(max_length=120, null=True)
+    description = models.TextField(null=True, validators=[MaxLengthValidator(120)])
     quantity = models.IntegerField(validators=[MaxValueValidator(100), MinValueValidator(0)])
