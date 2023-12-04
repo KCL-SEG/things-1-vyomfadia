8afb2b9ea12ea3cfc102432450bf7a3b5dd1f930
Vyom Fadia <vyom@tryterra.co>
2023-12-04 14:15:57 +0000

    feat: make things model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
+class Thing(models.Model):
+    """The basic thing model that is required for us to implement."""
+
+    name = models.CharField(unique=True, max_length=30)
+    description = models.TextField(max_length=120, null=True)
+    quantity = models.IntegerField(validators=[MaxValueValidator(99), MinValueValidator(1)])
